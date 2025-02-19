941baead12870a53540a2b3e1f69fe0539ff3b9f
dev-teng <vnctlcrn@gmail.com>
2025-02-19 17:59:36 +0800

    added emoji

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,7 @@
 function Home () {
   return (
     <div className="bg-secondary-subtle d-flex flex-column min-vh-100">
-      <h1 className="text-center m-5">From past pieces to future style 🧥👖</h1>
+      <h2 className="text-center m-5">From past pieces to future style 🧥👖</h2>
     </div>
    
   )
