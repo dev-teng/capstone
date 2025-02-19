a5941fa3abbcfec3c23c32ddb7529b673b00c275
dev-teng <vnctlcrn@gmail.com>
2025-02-19 17:35:36 +0800

    added X icon into hamburger

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,7 @@
 function Home () {
   return (
     <div className="bg-secondary-subtle d-flex flex-column min-vh-100">
-      <h1 className="text-center m-5">From past pieces to future style</h1>
+      <h1 className="text-center m-5">From past pieces to future style 🧥👖</h1>
     </div>
    
   )
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,12 +1,15 @@
 import { Outlet, Link } from "react-router-dom";
+import "./Header.css";
 function Navbar () {
   return(
     <>
      <nav className="navbar navbar-expand-lg bg-body-secondary">
       <div className="container-fluid bg-dark p-3">
         <Link to="/" className="navbar-brand text-light fw-bold">ReVibe</Link>
-        <button className="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+        <button className="navbar-toggler collapsed bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="toggler-icon top-bar"></span>
+          <span className="toggler-icon middle-bar"></span>
+          <span className="toggler-icon bottom-bar"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -24,7 +27,7 @@ function Navbar () {
       </div>
     </nav>
     <Outlet />
-    <footer className='p-3 text-center footer mt-auto fw-bold text-white bg-dark'>Copyright Allserved 2025</footer>
+    <footer className='p-3 text-center footer mt-auto fw-bold text-white bg-dark'>Copyright Allserved @ReVibe 2025</footer>
     </>
   )
 }
