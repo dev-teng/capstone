9e7b7f232888bf265464841968afb1a60ae031cc
dev-teng <vnctlcrn@gmail.com>
2025-02-20 18:09:40 +0800

    menu section

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,6 +1,9 @@
 function Menu () {
   return (
-    <h1>My menu</h1>
+    <div className="bg-secondary-subtle d-flex flex-column min-vh-100">
+          <h1 className="mt-5 p-5">My Menu</h1>
+    
+    </div>
   )
 }
 
